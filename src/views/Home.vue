2cd1c64e9719d83988db5f38a361f7b774db56db
jun39 <bbbbb12312339@gmail.com>
2020-05-19 22:11:42 +0900

    added MyButton components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyButton></MyButton>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import MyButton from '@/components/MyButton.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyButton
   }
 }
 </script>
